b8f1ec69ee512404bb452ba00b0ad75dc31b3868
LuisJimenezDev <LuisAJimenezA@gmail.com>
2013-04-16 15:13:58 -0500

    Commit Para Cargar el Curso de Django

---
+++ recetario/principal/admin.py
@@ -0,0 +1,7 @@
+#from principal.models import Bebida, Receta
+from principal.models import Receta, Comentario
+from django.contrib import admin
+
+#admin.site.register(Bebida)
+admin.site.register(Receta)
+admin.site.register(Comentario)
